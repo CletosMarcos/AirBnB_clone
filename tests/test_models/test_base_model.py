6a0b7c0c08899feeb9b889252839506c46cf6fd2
CletosMarcos <cletosmarcos@gmail.com>
2024-08-02 20:26:39 -0700

    initial test_base_model file

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines Tests for models.base_model Module"""
+
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """Unittests for BaseModel"""
+
+    def

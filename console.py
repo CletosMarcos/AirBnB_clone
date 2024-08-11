0f1101397e8b7efd998e5981da6b83870e07fba9
CletosMarcos <cletosmarcos@gmail.com>
2024-08-10 19:22:18 -0700

    doccument commands | add help methods

--- console.py
+++ console.py
@@ -11,7 +11,11 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, line):
         return True
 
+    def help_EOF(self):
+        print("Quit command to exit the program")
+
     do_quit = do_EOF
+    help_quit = help_EOF
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()

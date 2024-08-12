50fa5c4c7a2cda28125c51b67a396d2a524e2af3
CletosMarcos <cletosmarcos@gmail.com>
2024-08-11 19:30:26 -0700

    implement <show> command

--- console.py
+++ console.py
@@ -2,12 +2,15 @@
 """ contains the entry point of the command interpreter"""
 import cmd
 from models.base_model import BaseModel
-
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """implements the command interpreter"""
 
     prompt = "(hbnb)"
+
+    # available classes
+    classes = ["BaseModel"]
 
     def do_EOF(self, line):
         return True
@@ -22,11 +25,9 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, obj):
-        classes = ["BaseModel"]
-
         if obj:
             # for k, v in classes.items():
-            if obj in classes:
+            if obj in self.classes:
                 instance = BaseModel()
                 instance.save()
                 print(instance.id)
@@ -38,6 +39,37 @@ class HBNBCommand(cmd.Cmd):
     def help_create(self):
         print("".join(["Creates a new instance of BaseModel,",
                     " saves it (to the JSON file) and prints the id"]))
+        print("[Usage]: create <className>\n")
+
+    def do_show(self, obj):
+        objt = obj.partition(" ")
+        class_name = objt[0]
+        obj_id = objt[2]
+
+        if not class_name:
+            print("** class name missing **")
+            return
+        if class_name not in self.classes:
+            print("** class doesn't exist **")
+            return
+        if not obj_id:
+            print("** instance id missing **")
+            return
+
+        try:
+            list_obj = storage.all()
+            for k, v in list_obj.items():
+                if obj_id == k.split(".")[1]:
+                    print(v)
+                    return
+            print("** no instance found **")
+        except:
+            pass
+    
+    def help_show(self):
+        print("".join(["Prints the string representation of an instance",
+                " based on the class name and id"]))
+        print("[Usage]: show <className> <objectId>\n")
 
 
 if __name__ == "__main__":

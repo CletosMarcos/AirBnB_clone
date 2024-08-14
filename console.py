87ca5554075a17e2b0f43ea4ffd246537df36128
CletosMarcos <cletosmarcos@gmail.com>
2024-08-13 19:59:42 -0700

    update the console to allow those actions: <show>, <create>, <destroy>, <update> and <all> with all classes created previously

--- console.py
+++ console.py
@@ -3,6 +3,11 @@
 import cmd
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+from models.review import Review
 from models import storage
 
 
@@ -12,7 +17,8 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
 
     # available classes
-    classes = {"BaseModel": BaseModel, "User": User}
+    classes = {"BaseModel": BaseModel, "User": User, "Place": Place,
+                "State": State, "City": City, "Amenity": Amenity, "Review": Review}
 
     def do_EOF(self, line):
         return True
